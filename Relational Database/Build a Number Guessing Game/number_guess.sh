ea3a00614d9e9af260837a1ba775fa079c29d324
Ahmed@Atef <47149282+Ahmed3atef@users.noreply.github.com>
2025-11-02 00:26:59 +0200

    Refactor number guessing game script

--- Relational Database/Build a Number Guessing Game/number_guess.sh
+++ Relational Database/Build a Number Guessing Game/number_guess.sh
@@ -1,74 +1,105 @@
-#! /bin/bash
+#!/bin/bash
+# Define PSQL variable
+PSQL="psql --username=freecodecamp --dbname=number_guess -X -t --no-align -c"
 
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-DISPLAY() {
-  echo -e "\n~~~~~ Number Guessing Game ~~~~~\n" 
+# Function to generate a random number
+GENERATE_RANDOM_NUMBER(){
+  RN=$((RANDOM % 1000 + 1))
+  echo $RN
+}
 
-  #get username
-  echo "Enter your username:"
-  read USERNAME
+# Function to register a new user
+REGISTER_NEW_USER() {
+    INSERT_NEW_USER=$($PSQL "INSERT INTO users(username) VALUES('$1')")
+    echo "Welcome, $1! It looks like this is your first time here."
+}
 
-  #get username from db
-  USER_ID=$($PSQL "select u_id from users where name = '$USERNAME'")
+# Function to merge the get user status for db
+GET_USER_STATS(){
+  $PSQL "
+  SELECT 
+    u.user_id, 
+    u.username, 
+    COUNT(g.game_id), 
+    MIN(g.number_of_gusses) 
+  FROM users as u 
+  FULL JOIN games as g 
+  USING(user_id) 
+  WHERE u.username = '$1' 
+  GROUP BY u.user_id, u.username
+  "
+}
 
-  #if user present
-  if [[ $USER_ID ]]; then
-    #get games played
-    GAMES_PLAYED=$($PSQL "select count(u_id) from games where u_id = '$USER_ID'")
+START_GAME(){
+  # get what returned from the function into var called NUMBER_GENERATED
+  SECRET_NUMBER=$(GENERATE_RANDOM_NUMBER)
+  local GUESS_COUNT=0
 
-    #get best game (guess)
-    BEST_GUESS=$($PSQL "select min(guesses) from games where u_id = '$USER_ID'")
+  echo "Guess the secret number between 1 and 1000:"
+  
+  while read GUESS; do
+      if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+          echo "That is not an integer, guess again:"
+          continue
+      fi
+      
+      GUESS_COUNT=$((GUESS_COUNT + 1))
 
-    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GUESS guesses."
+      if (( GUESS < SECRET_NUMBER )); then
+          echo "It's higher than that, guess again:"
+      elif (( GUESS > SECRET_NUMBER )); then
+          echo "It's lower than that, guess again:"
+      else
+        break # Correct Guess!
+      fi
+  done
+
+  # Save game results using the global USER_ID
+  INSERT_GAME=$($PSQL "INSERT INTO games(user_id, number_of_gusses) VALUES($1, $GUESS_COUNT)")
+  if [[ $INSERT_GAME = 'INSERT 0 1' ]]
+  then
+    echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
   else
-    #if u_name not present in db
-    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-
-    #insert to users table
-    INSERTED_TO_USERS=$($PSQL "insert into users(name) values('$USERNAME')")
-    #get user_id
-    USER_ID=$($PSQL "select u_id from users where name = '$USERNAME'")
-    # echo $USER_ID
+    exit 1
   fi
 
-  GAME
 }
 
-GAME() {
-  #secret number
-  SECRET=$((1 + $RANDOM % 1000))
+GET_USER(){
+  # Check user existence: Returns ID if found, empty if not.
+  local CHECK_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$1'")
 
-  #count guesses
-  TRIES=0
 
-  #guess number
-  GUESSED=0
-  echo -e "\nGuess the secret number between 1 and 1000:"
+  if [[ -z $CHECK_ID ]] # User not found
+  then
+    REGISTER_NEW_USER "$1"
+  fi
 
-  while [[ $GUESSED = 0 ]]; do
-    read GUESS
+  # Get user stats (for existing or newly registered user)
+  local USER_INFO=$(GET_USER_STATS "$1")
 
-    #if not a number
-    if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
-      echo -e "\nThat is not an integer, guess again:"
-    #if correct guess
-    elif [[ $SECRET = $GUESS ]]; then
-      TRIES=$(($TRIES + 1))
-      echo -e "\nYou guessed it in $TRIES tries. The secret number was $SECRET. Nice job!"
-      #insert into db
-      INSERTED_TO_GAMES=$($PSQL "insert into games(u_id, guesses) values($USER_ID, $TRIES)")
-      GUESSED=1
-    #if greater
-    elif [[ $SECRET -gt $GUESS ]]; then
-      TRIES=$(($TRIES + 1))
-      echo -e "\nIt's higher than that, guess again:"
-    #if smaller
-    else
-      TRIES=$(($TRIES + 1))
-      echo -e "\nIt's lower than that, guess again:"
-    fi
-  done
+
+  
+  IFS="|" read USER_ID USER_NAME GAMES_PLAYED MINI_GUSSES <<< "$USER_INFO"
+  # Check if the user is returning (GAMES_PLAYED > 0 and a best score exists)
+  if [[ $GAMES_PLAYED -gt 0 && -n $MINI_GUSSES ]]; then
+    echo "Welcome back, $USER_NAME! You have played ${GAMES_PLAYED:-0} games, and your best game took ${MINI_GUSSES:-0} guesses."
+  fi
+
+  START_GAME $USER_ID
+
 }
 
-DISPLAY
+START(){
+  echo -e "Enter your username:"
+  read USERNAME
+  if [[ -z $USERNAME || ${#USERNAME} -gt 22 ]]; then
+      echo "Invalid username. Please try again."
+      exit 1
+  fi
+  # Call GET_USER and pass the username
+  GET_USER "$USERNAME"
+}
+
+START
